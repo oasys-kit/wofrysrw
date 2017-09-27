8f1070c9bfd7794bc6e530798f9d389e91d04e80
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-09-27 09:49:25 +0200

    Bug Fixing

--- wofrysrw/storage_ring/srw_electron_beam.py
+++ wofrysrw/storage_ring/srw_electron_beam.py
@@ -37,25 +37,39 @@ class SRWElectronBeam(ElectronBeam, SRWElectronBeamDecorator):
                  energy_spread = 0.0,
                  current = 0.1,
                  number_of_bunches = 400,
+                 moment_x=0.0,
+                 moment_xp=0.0,
+                 moment_y=0.0,
+                 moment_yp=0.0,
+                 moment_z=0.0,
                  moment_xx=0.0,
                  moment_xxp=0.0,
                  moment_xpxp=0.0,
                  moment_yy=0.0,
                  moment_yyp=0.0,
-                 moment_ypyp=0.0,
-                 drift_distance=0.0):
-        super().__init__(energy_in_GeV, energy_spread, current, number_of_bunches, moment_xx, moment_xxp, moment_xpxp, moment_yy, moment_yyp, moment_ypyp)
+                 moment_ypyp=0.0):
+        super().__init__(energy_in_GeV,
+                         energy_spread,
+                         current,
+                         number_of_bunches,
+                         moment_xx,
+                         moment_xxp,
+                         moment_xpxp,
+                         moment_yy,
+                         moment_yyp,
+                         moment_ypyp)
 
-        self._drift_distance = drift_distance
+        self._moment_x = moment_x
+        self._moment_xp = moment_xp
+        self._moment_y = moment_y
+        self._moment_yp = moment_yp
+        self._moment_z = moment_z
 
     def set_moments_from_electron_beam_geometrical_properties(self, electron_beam_geometrical_properties = SRWElectronBeamGeometricalProperties()):
         self.set_sigmas_all(sigma_x=electron_beam_geometrical_properties._electron_beam_size_h,
                             sigma_xp=electron_beam_geometrical_properties._electron_beam_divergence_h,
                             sigma_y=electron_beam_geometrical_properties._electron_beam_size_v,
                             sigma_yp=electron_beam_geometrical_properties._electron_beam_divergence_v)
-
-    def set_drift_distance(self, drift_distance):
-        self._drift_distance = drift_distance
 
     def get_electron_beam_geometrical_properties(self):
         x, xp, y, yp = self.get_sigmas_all()
@@ -67,12 +81,12 @@ class SRWElectronBeam(ElectronBeam, SRWElectronBeamDecorator):
     def to_SRWLPartBeam(self):
         srw_electron_beam = SRWLPartBeam()
         srw_electron_beam.Iavg           = self._current
-        srw_electron_beam.partStatMom1.x = 0.0
-        srw_electron_beam.partStatMom1.y = 0.0
 
-        srw_electron_beam.partStatMom1.z = 0
-        srw_electron_beam.partStatMom1.xp = 0
-        srw_electron_beam.partStatMom1.yp = 0
+        srw_electron_beam.partStatMom1.x = self._moment_x
+        srw_electron_beam.partStatMom1.y = self._moment_y
+        srw_electron_beam.partStatMom1.z = self._moment_z
+        srw_electron_beam.partStatMom1.xp = self._moment_xp
+        srw_electron_beam.partStatMom1.yp = self._moment_yp
         srw_electron_beam.partStatMom1.gamma = self.gamma()
 
         #2nd order statistical moments:
@@ -84,8 +98,6 @@ class SRWElectronBeam(ElectronBeam, SRWElectronBeamDecorator):
         srw_electron_beam.arStatMom2[5] = self._moment_ypyp #<(y'-y'0)^2>
         srw_electron_beam.arStatMom2[10] = self._energy_spread**2 #<(E-E0)^2>/E0^2
 
-        srw_electron_beam.drift(self._drift_distance)
-
         return srw_electron_beam
 
     @classmethod
@@ -93,13 +105,17 @@ class SRWElectronBeam(ElectronBeam, SRWElectronBeamDecorator):
         srw_electron_beam = SRWElectronBeam(energy_spread = numpy.sqrt(srw_part_beam.arStatMom2[10]),
                                             current = srw_part_beam.Iavg,
                                             number_of_bunches = number_of_bunches,
+                                            moment_x=srw_part_beam.partStatMom1.x,
+                                            moment_y=srw_part_beam.partStatMom1.y,
+                                            moment_z=srw_part_beam.partStatMom1.z,
+                                            moment_xp=srw_part_beam.partStatMom1.xp,
+                                            moment_yp=srw_part_beam.partStatMom1.yp,
                                             moment_xx  = srw_part_beam.arStatMom2[0],
                                             moment_xxp = srw_part_beam.arStatMom2[1],
                                             moment_xpxp= srw_part_beam.arStatMom2[2],
                                             moment_yy  = srw_part_beam.arStatMom2[3],
                                             moment_yyp = srw_part_beam.arStatMom2[4],
-                                            moment_ypyp= srw_part_beam.arStatMom2[5],
-                                            drift_distance=srw_part_beam.z)
+                                            moment_ypyp= srw_part_beam.arStatMom2[5])
 
         srw_electron_beam.set_energy_from_gamma(srw_part_beam.partStatMom1.gamma)
 

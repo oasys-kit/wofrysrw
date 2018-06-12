32ed390fd2052e044275fc57aa13681a0b9e2a39
Luca Rebuffi <luca.rebuffi@elettra.eu>
2018-06-12 12:23:18 +0200

    SRW in OASYS: Version 0.1.18, Missing parameters in Undulator

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except AttributeError:
 
 NAME = 'wofrysrw'
 
-VERSION = '1.0.16'
+VERSION = '1.0.17'
 ISRELEASED = False
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon) for SRW library'
--- wofrysrw/storage_ring/magnetic_structures/srw_undulator.py
+++ wofrysrw/storage_ring/magnetic_structures/srw_undulator.py
@@ -9,17 +9,41 @@ class SRWUndulator(Undulator, SRWMagneticStructure):
                  K_vertical = 0.0,
                  K_horizontal = 0.0,
                  period_length = 0.0,
-                 number_of_periods = 1):
+                 number_of_periods = 1,
+                 initial_phase_vertical = 0.0, # initial phase [rad]
+                 initial_phase_horizontal = 0.0, # initial phase [rad]
+                 symmetry_vs_longitudinal_position_vertical = 1, # symmetry vs longitudinal position 1 - symmetric (B ~ cos(2*Pi*n*z/per + ph)) , -1 - anti-symmetric (B ~ sin(2*Pi*n*z/per + ph))
+                 symmetry_vs_longitudinal_position_horizontal = -1, # symmetry vs longitudinal position 1 - symmetric (B ~ cos(2*Pi*n*z/per + ph)) , -1 - anti-symmetric (B ~ sin(2*Pi*n*z/per + ph))
+                 coefficient_for_transverse_dependence_vertical = 1.0, # coefficient for transverse depenednce B*cosh(2*Pi*n*a*y/per)*cos(2*Pi*n*z/per + ph)
+                 coefficient_for_transverse_dependence_horizontal = 1.0 # coefficient for transverse depenednce B*cosh(2*Pi*n*a*y/per)*cos(2*Pi*n*z/per + ph)
+                ):
         Undulator.__init__(self, K_vertical, K_horizontal, period_length, number_of_periods)
+
+        self.initial_phase_vertical = initial_phase_vertical
+        self.initial_phase_horizontal = initial_phase_horizontal
+        self.symmetry_vs_longitudinal_position_vertical = symmetry_vs_longitudinal_position_vertical
+        self.symmetry_vs_longitudinal_position_horizontal = -1, symmetry_vs_longitudinal_position_horizontal
+        self.coefficient_for_transverse_dependence_vertical = coefficient_for_transverse_dependence_vertical
+        self.coefficient_for_transverse_dependence_horizontal = coefficient_for_transverse_dependence_horizontal
 
     def get_SRWMagneticStructure(self):
         magnetic_fields = []
 
         if self._K_vertical > 0.0:
-            magnetic_fields.append(SRWLMagFldH(1, 'v', self.magnetic_field_vertical(), 0, 1, 1))
+            magnetic_fields.append(SRWLMagFldH(1,
+                                               'v',
+                                               self.magnetic_field_vertical(),
+                                               self.initial_phase_vertical,
+                                               self.symmetry_vs_longitudinal_position_vertical,
+                                               self.coefficient_for_transverse_dependence_vertical))
 
         if self._K_horizontal > 0.0:
-            magnetic_fields.append(SRWLMagFldH(1, 'h', self.magnetic_field_horizontal(), 0, -1, 1))
+            magnetic_fields.append(SRWLMagFldH(1,
+                                               'h',
+                                               self.magnetic_field_horizontal(),
+                                               self.initial_phase_horizontal,
+                                               self.symmetry_vs_longitudinal_position_horizontal,
+                                               self.coefficient_for_transverse_dependence_horizontal))
 
         return SRWLMagFldU(magnetic_fields,
                            self._period_length,

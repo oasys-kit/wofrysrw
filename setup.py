061a3ce9c81784446c500d0b193ab0af6ad9e42d
lucarebuffi <lrebuffi@anl.gov>
2019-11-07 16:45:03 -0600

    New Features

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'wofrysrw'
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY for SRW library'

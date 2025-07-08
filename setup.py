ceb1fd17c74d5580213129a975213c94da80d943
lrebuffi <lrebuffi@anl.gov>
2025-07-08 10:52:42 -0500

    bug fixing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'wofrysrw'
-VERSION = '1.1.27'
+VERSION = '1.1.28'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY for SRW library'

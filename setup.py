2f5d81e9f88f77b3863c34ee3c5e5744ed5ef745
lrebuffi <lrebuffi@anl.gov>
2025-10-15 12:20:55 -0500

    compatibility with higher versions of scipy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'wofrysrw'
-VERSION = '1.1.33'
+VERSION = '1.1.34'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY for SRW library'
